582d4f4c63bba15a965a8619e9d806e8f2f39ea0
bittylokhondiaye <labitty189@outlook.fr>
2019-09-27 12:27:32 +0000

    first commit et dernier

---
+++ src/main/java/com/devweb/rh/repository/UserRepository.java
@@ -0,0 +1,19 @@
+package com.devweb.rh.repository;
+
+import com.devweb.rh.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
+    Boolean existsByUsername(String username);
+    Boolean existsByEmail(String email);
+    //@Query("SELECT u FROM User u WHERE u.LOGIN is NULL")
+    //public List<User> users();
+}
